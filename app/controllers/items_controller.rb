700efe7f023ddc15c73fb0dbe435f5db686fa675
Alfreda Tsai <alfredatsai@gmail.com>
2019-05-15 16:11:31 -0600

    all basic objectives

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,59 @@
+class ItemsController < ApplicationController
+  before_action :set_department
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @items = @department.items
+  end
+
+  def show
+  end
+
+  def new
+    @item = @department.items.new
+    render partial: "form"
+  end
+
+  def create
+    @item = @department.items.new(item_params)
+
+    if @item.save
+      redirect_to department_item_path(@department.id, @item.id)
+    else
+      render partial: "form"
+    end
+  end
+
+  def edit
+    render partial: "form"
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to department_item_path(@department, @item)
+    else
+      render partial: "form"
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to department_items_path(@department)
+  end
+
+
+  private
+
+  def set_department
+    @department = Department.find(params[:department_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
+
+end
